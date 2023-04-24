a85f734470857817dc47bfe84b74e5c8f95d5ecd
Andy Noton <andy.noton.freelancer@guardian.co.uk>
2023-04-24 14:57:02 +0100

    fix dev deps

--- zuora-oracle-fusion/src/canonical-config.d.ts
+++
@@ -1,3 +0,0 @@
-// module declarations so we can import canonical config code using `npm link`
-declare module 'canonical-config/dist/zuora-api.js';
-declare module 'canonical-config/dist/app-parameters.js';
